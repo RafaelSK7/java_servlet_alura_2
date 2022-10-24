201a35f5ecf72d002e3c896d18b0274ab7e375a2
Rafael Kiss <rafaelskiss1@hotmail.com>
2022-10-24 17:03:51 -0300

    Modulo 2 completo

--- gerenciador/src/br/com/alura/gerenciador/acao/NovaEmpresaForm.java
+++ gerenciador/src/br/com/alura/gerenciador/acao/NovaEmpresaForm.java
@@ -1,8 +1,10 @@
 package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,23 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
-public class MostraEmpresa implements Acao {
+public class NovaEmpresaForm implements Acao {
 
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		String paramId = request.getParameter("id");
-		Integer id = Integer.valueOf(paramId);
-		
-		Banco banco = new Banco();
-		
-		Empresa empresa = banco.buscaEmpresaPelaId(id);
-		
-		System.out.println(empresa.getNome());
-
-		request.setAttribute("empresa", empresa);
-		
-		return "forward:formAlteraEmpresa.jsp";
+		return "forward:formNovaEmpresa.jsp";
 	}
-	
+
 }
